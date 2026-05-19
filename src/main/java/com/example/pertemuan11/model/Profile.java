1ddf80094624873f439952f2e14cb4dc00c9cd93
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2026-05-19 14:14:01 +0700

    Profile.Java

---
+++ src/main/java/com/example/pertemuan11/model/Profile.java
@@ -0,0 +1,24 @@
+package com.example.pertemuan11.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+    @Id
+    private String Id;
+    private String nama;
+    private String nama;
+    private String alamat;
+    @OnetoOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
